0e1656a9ce5783186eca1fc798fc6466ea91b346
K V Madhan Raj <83569612+Madhan-404@users.noreply.github.com>
2022-07-10 20:16:03 +0530

    completed

--- Exercises/src/bin/a29a.rs
+++ Exercises/src/bin/a29a.rs
@@ -16,4 +16,20 @@ use this function for i32 and f32 types
 
 fn main() {
 
+let res1 = greater_no::<i32>(1, 20, 300);
+let res2 = greater_no::<f32>(1.0, 20.0, 300.0);
+
+println!("{} {}",res1,res2);
+
+}
+
+fn greater_no<T:PartialOrd>(a:T,b:T,c:T) ->T {
+    
+    if a>b && a>c {
+        return a ;
+    }else if b>a && b>c {
+        return b ;
+    }else {
+        return c ;
+    }
 }
