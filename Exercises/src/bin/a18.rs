45c919d2674c89adf96f52d4806dc28868d534d0
K V Madhan Raj <83569612+Madhan-404@users.noreply.github.com>
2022-07-13 23:25:41 +0530

    completed

--- Exercises/src/bin/a18.rs
+++ Exercises/src/bin/a18.rs
@@ -1,19 +1,39 @@
 // Topic: Result
 //
 // Requirements:
-// * Determine if a customer is able to make a restricted purchase
-// * Restricted purchases require that the age of the customer
+// * Determine if a customer is able to make a restricted  entry into a movie theatre
+// * Restricted entry require that the age of the customer
 //   is at least 21
 //
 // Notes:
 // * Use a struct to store at least the age of a customer
-// * Use a function to determine if a customer can make a restricted purchase
+// * Use a function to determine if a customer can make a restricted entry
 // * Return a result from the function
-// * The Err variant should detail the reason why they cannot make a purchase
+// * The Err variant should detail the reason why they cannot make a entry
 
 
 
+// struct Customer {
+//     age:i32
+// }
 
+fn entry(Age: i32) -> Result<bool,String>{
+    if Age >=21 {
+        Ok(true)
+    }else {
+         return Err("Your Age is less than 21 :(".to_string()) ;
+    }
+}
 
+fn main() {
 
-fn main() {}
+    
+    let Entry = entry(28);
+
+    match Entry {
+        Ok(d) => println!("{}",d),
+        Err(e) => println!("{}",e)
+    }
+
+}
+
