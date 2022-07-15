b8cd2c632a240f1eaba954f082754255e3875fde
K V Madhan Raj <83569612+Madhan-404@users.noreply.github.com>
2022-07-16 00:35:15 +0530

    completed

--- Exercises/src/bin/a18b.rs
+++ Exercises/src/bin/a18b.rs
@@ -22,4 +22,88 @@
 // * Print whether the employee may access the building
 //   * Must use a function that utilizes the question mark operator to do this
 
-fn main() {}
+
+// Use an enum to represent all types of employees
+enum Employees{
+    Maintenance,
+    Marketing,
+    Managers,
+    LineSupervisors,
+    Cooks,
+    AssemblyTech
+}
+
+// * Use a struct to store the employee type and whether they are
+//   still employed -> status -> Active / Terminated
+
+
+enum Status {
+    Active,
+    Terminated
+}
+struct EmplyeeStatus {
+    //
+    emp:Employees,
+    status:Status
+}
+
+
+// * Use a function that returns a Result to determine if the employee
+//   may enter the building
+// (ES)-> Result<T,E> // T? E? 
+//-> 
+/*
+    Maintenance, =>
+    Marketing,=> 
+    Managers,=>
+    _=>    Err()
+           
+*/
+
+fn enter(es:EmplyeeStatus) -> Result<(),String> {
+     
+
+    match es.status {
+        Status::Terminated => return Err("Status is terminated".to_owned()),
+        _=> ()
+    }
+
+
+    match es.emp{
+        Employees::Maintenance => Ok(()),
+        Employees::Managers => Ok(()),
+        Employees::Marketing => Ok(()),
+        _=> Err("Acees denied".to_string())
+    }
+
+
+}
+
+// * Print whether the employee may access the building
+//   * Must use a function that utilizes the question mark operator to do this
+
+
+// fn  ? 
+fn print(es:EmplyeeStatus) -> Result<(),String> {
+
+    let res = enter(es)?;
+
+    println!("access is ok");
+
+    Ok(())
+
+}
+
+
+fn main() {
+
+    let es = EmplyeeStatus{
+           emp:Employees::Managers,
+           status:Status::Active
+    };
+
+    let res = print(es);
+
+
+    
+}
